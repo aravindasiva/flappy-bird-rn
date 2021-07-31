7ac187beb154daf207bd344cf1371a209c2faf9f
aravindasiva <aravinda.siva@pego.co.uk>
2021-07-31 19:24:27 +0100

    add floor component and make it static

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import entities from "./entities";
+import Physics from './physics';
 
 export default function App() {
+  const [running, setRunning] = useState(false)
+  useEffect(() => {
+    setRunning(true)
+  }, [])
   return (
     <View style={{flex: 1}}>
       <GameEngine 
+        systems={[Physics]}
         entities={entities()}
+        running = {running}
         style={{position: 'absolute', top: 0, bottom: 0, right: 0, left:0}}>
 
       </GameEngine>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" hidden={true} />
     </View>
   );
 }
